5fb06e3a44563e8f66558d15517cdbe42353a539
Knstoey03 <ttoeyq@hotmail.com>
2024-03-18 18:42:07 +0700

    Final Examination

---
+++ catalog_app/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+
+    path('', views.homepage, name='homepage'),
+    path('product_list', views.product_list, name='product list'),
+    path('product_detail', views.product_detail, name='product detail'),
+    path('back_login', views.back_login, name='back_login'),
+    path('transfer', views.transfer, name='transfer'),
+    path('register_account', views.register_account, name='register_account'),
+    path('check_balance', views.check_balance, name='check_balance'),
+    path('transfer_list', views.transfer_list, name='transfer_list'),
+]
